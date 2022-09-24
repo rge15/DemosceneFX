ac4c944c728f8eb12e8a0842e670f79f1ad5da69
rge15 <rge15.ua@gmail.com>
2022-09-24 11:03:50 +0200

    [update] Nos falta solo los calculos de colores pero lo tenemos

--- fx/Fire/src/main.cpp
+++ fx/Fire/src/main.cpp
@@ -16,7 +16,7 @@ int main()
 	ptc_open("window", WIDTH_SCREEN, HEIGHT_SCRREN);
 
 	uint32_t i = 0;
-	uint32_t j = WIDTH_SCREEN * (HEIGHT_SCRREN - 1); 
+	uint32_t j = WIDTH_SCREEN * (HEIGHT_SCRREN - 2); 
 	uint32_t* _ptrScreen = _screen + TOTAL_PIXELS - 1;
 	for(;;)
 	{
@@ -29,22 +29,33 @@ int main()
 			--_ptrScreen;
 		};
 
-		for(; i > 0 ; i--)
-		{
-			uint32_t rngVal = rand();
+		_ptrScreen = _screen + TOTAL_PIXELS - 1;
 
+		for( i = TOTAL_PIXELS ; i > WIDTH_SCREEN*5 ; i--)
+		{
+
+			uint32_t* prev = _ptrScreen-1;
+			uint32_t* next = _ptrScreen+1;
+			uint32_t* above = _ptrScreen-WIDTH_SCREEN;
+			uint32_t  fireValue { 0 };
 			auto moduleResult = i%WIDTH_SCREEN;
+
+			//TODO : Las sumas suman todos los valores y mezclan los RGB Values
+			//TODO : Hacer una macro/funcion que pille los valores R y G por separado 
+			//TODO : que los sume y que haga modulo de 255 y los ponga donde toque
+			//TODO : Valdría con hacer >> 8 ,<< 16 Y >> 8 ||  >> 8, << 8 y obtener el minimo comun multiplo de 2 ^ 23 (creo que con puesta XOR)
 			if( moduleResult == 0 )
 			{
-				*_ptrScreen = ((rngVal%122 + 122) << 8 ) + (rngVal%122) ;
+				fireValue = *(_ptrScreen) + *(prev) + *(above)  ;
 			}else if ( moduleResult == 1)
 			{
-				*_ptrScreen = ((rngVal%122 + 122) << 8 ) + (rngVal%122) ;
+				fireValue = *(_ptrScreen) + *(next) + *(above)  ;
 			}else
 			{
-				*_ptrScreen = (rngVal%122) ;
+				fireValue = *(_ptrScreen) + *(prev) + *(above) + *(next) ;
 			}
-			
+			fireValue /= 3;
+			*above = fireValue;
 
 			--_ptrScreen;
 		};
