ecea0af0c5c918b884e43826f3ccba6660ee2814
rge15 <rge15.ua@gmail.com>
2022-09-24 16:01:59 +0200

    [FIRE] Fire FX with some variants

--- fx/Fire/src/main.cpp
+++ fx/Fire/src/main.cpp
@@ -15,16 +15,24 @@ int main()
 {
 	ptc_open("window", WIDTH_SCREEN, HEIGHT_SCRREN);
 
-	uint32_t i = 0;
-	uint32_t j = WIDTH_SCREEN * (HEIGHT_SCRREN - 2); 
+	uint32_t i { 0 };
+	uint32_t j = WIDTH_SCREEN * (HEIGHT_SCRREN - 3);
 	uint32_t* _ptrScreen = _screen + TOTAL_PIXELS - 1;
+
 	for(;;)
 	{
 
 		for(i = TOTAL_PIXELS ; i > j ; i--)
 		{
 			uint32_t rngVal = rand();
-			*_ptrScreen = ((rngVal%122 + 122) << 16 ) + ((rngVal%122) << 8) ;
+
+			*_ptrScreen = (rngVal%255) << 16;
+
+			if(rngVal%3 == 1)
+				*_ptrScreen = ((rngVal%122+122) << 16) + ((rngVal%255) << 8);
+
+			if(rngVal%9 == 1)
+				*_ptrScreen = 0;
 
 			--_ptrScreen;
 		};
@@ -37,24 +45,53 @@ int main()
 			uint32_t* prev = _ptrScreen-1;
 			uint32_t* next = _ptrScreen+1;
 			uint32_t* above = _ptrScreen-WIDTH_SCREEN;
+			
+			uint32_t prevVal = *prev;
+			uint32_t nextVal = *next;
+			uint32_t aboveVal = *above;
+			uint32_t thisVal = *_ptrScreen;
+			
 			uint32_t  fireValue { 0 };
 			auto moduleResult = i%WIDTH_SCREEN;
-
-			//TODO : Las sumas suman todos los valores y mezclan los RGB Values
-			//TODO : Hacer una macro/funcion que pille los valores R y G por separado 
-			//TODO : que los sume y que haga modulo de 255 y los ponga donde toque
-			//TODO : Valdría con hacer >> 8 ,<< 16 Y >> 8 ||  >> 8, << 8 y obtener el minimo comun multiplo de 2 ^ 23 (creo que con puesta XOR)
-			if( moduleResult == 0 )
+			
+			if( moduleResult == 0 ) //RIGHT BAND SCREEN
 			{
-				fireValue = *(_ptrScreen) + *(prev) + *(above)  ;
-			}else if ( moduleResult == 1)
+				fireValue = thisVal + prevVal + aboveVal;
+				fireValue /= 3;
+			}else if ( moduleResult == 1) //LEFT BAND SCREEN
 			{
-				fireValue = *(_ptrScreen) + *(next) + *(above)  ;
-			}else
+				fireValue = thisVal + nextVal + aboveVal;
+				fireValue /= 3;
+			}else //MIDDLE SCREEN
 			{
-				fireValue = *(_ptrScreen) + *(prev) + *(above) + *(next) ;
+				fireValue = thisVal + prevVal + aboveVal + nextVal ;
+				fireValue >>= 2;
 			}
-			fireValue /= 3;
+
+			//?VARIANT I?
+			// fireValue &= 0x00FFFF00;
+
+			//?VARIANT II?
+			// fireValue &= 0x00FFFF00;
+
+			// if(fireValue <= 0x00300000)
+				// fireValue = 0;
+
+			//?VARIANT II?
+			// if(fireValue <= 0x00300000)
+				// fireValue = 0;
+			
+			//?VARIANT II?
+			fireValue &= 0x00FF0000;
+
+			if(fireValue <= 0x00300000)
+				fireValue = 0;
+
+
+			//TODO : Aquí tengo que controlar que el valor de verde no supere al del rojo
+			//TODO : Tambien tengo controlar el fade out
+
+
 			*above = fireValue;
 
 			--_ptrScreen;
