2f349775de34c0fe8e8f0b592d8eb5bb8ff6e2be
rge15 <rge15.ua@gmail.com>
2022-10-24 13:33:19 +0200

    [sprites] Adding sprites configuration

--- fx/DemoEngine/src/main.cpp
+++ fx/DemoEngine/src/main.cpp
@@ -8,14 +8,25 @@ int main()
 	auto& drawer = engine.getDrawer();
 
 	// auto& sprBuffer = drawer.addBuffer<SpriteFX>( 10 );
-	// auto& B1 = sprBuffer.addSprite("img/B2.png", 10);
-	// auto& B2 = sprBuffer.addSprite("img/B2.png", 10);
-	// auto& Y = sprBuffer.addSprite("img/Y2.png", 10);
-	// auto& O = sprBuffer.addSprite("img/O2.png", 10);
+	// auto& B1 = sprBuffer.addSprite("img/B.png", 10);
+	// auto& B2 = sprBuffer.addSprite("img/B.png", 10);
+	// auto& Y = sprBuffer.addSprite("img/Y.png", 10);
+	// auto& O = sprBuffer.addSprite("img/O.png", 1);
+	// auto& I = sprBuffer.addSprite("img/I.png", 10);
+	// auto& T = sprBuffer.addSprite("img/T.png", 10);
 
+	// auto& M = sprBuffer.addSprite("img/M.png", 10);
+	// auto& K = sprBuffer.addSprite("img/K.png", 10);
+	// auto& ONE = sprBuffer.addSprite("img/1.png", 10);
 
 	// B1.setPos( 200,100 );
 	// B1.setSpeed( 1, 0 );
+
+	// I.setPos( 280,120 );
+	// I.setSpeed( 1, 0 );
+	
+	// T.setPos( 330,120 );
+	// T.setSpeed( 1, 0 );
 
 	// B2.setPos( 250, 185 );
 	// B2.setSpeed( 1, 0 );
@@ -26,13 +37,19 @@ int main()
 	// Y.setPos( 381, 200 );
 	// Y.setSpeed( 1, 0 );
 
+	// M.setPos( 50, 0 );
+	// K.setPos( 200, 0 );
+	// ONE.setPos( 300, 0 );
+
+
 
 	// sprBuffer.setBufferFX( std::make_unique<FireFX>( 10, *(drawer._src.get()) ) );
 
 	// drawer.addBuffer<PlasmaFX>( 3 );
 	// drawer.addBuffer<FireUpDownFX>( 20 );
 
-	drawer.addTunnelBuffer( 10, "img/Tunel_1.png" );
+	drawer.addTunnelBuffer( 5, "img/tun_1.png" );
+	drawer.addTunnelBuffer( 5, "img/tun_2.png" );
 	drawer.addBuffer<MoireFX>( 3 );
 	drawer.addBuffer<MoireTimedFX>( 3 );
 	drawer.addBuffer<MoireExtendFX>( 3 );
